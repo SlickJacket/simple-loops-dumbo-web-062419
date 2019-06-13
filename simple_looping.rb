b1aa49629bfdbae411b71ab21d38f2fa7e9b662f
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-13 00:31:43 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,13 +12,10 @@ def loop_iterator(number_of_times)
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"
     counter += 1
-    break if counter == number_of_times 
+    break if counter == number_of_times
   end
 
   end
-phrase = "Welcome to Flatiron School's Web Development Course!"
-
-end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
